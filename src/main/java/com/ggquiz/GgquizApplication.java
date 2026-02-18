c91397fddb4d1244f44b98bb3d4a844c58da89b9
reynanwq <reynanwq@gmail.com>
2026-02-17 21:33:42 -0300

    feat: initialize Spring Boot application following Clean Code principles

---
+++ src/main/java/com/ggquiz/GgquizApplication.java
@@ -0,0 +1,13 @@
+package com.ggquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ggquiz/GgquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
